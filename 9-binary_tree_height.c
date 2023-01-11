60d2353c7c99372566dbea04c3b91456bc83a577
NMenendez24 <nahuelmt24@gmail.com>
2023-01-11 09:44:50 -0800

    Task 9 Fixed NULL case

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,17 @@
 #include "binary_trees.h"
 
+/**
+  * binary_tree_height - Calculates the height of a binary tree
+  * Return: Height
+  * @tree: Binary Tree
+  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t h = 0, h2 = 0;
 
+	if (!tree)
+		return (0);
 	h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	h2 = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (h > h2 ? h : h2);
